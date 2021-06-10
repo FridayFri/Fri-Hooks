5f3394ffe09c608362441e03a2e2dac4f00a32e5
shengqiwu <shengqiwu@codemao.cn>
2021-06-11 01:44:25 +0800

    feat: useSize

---
+++ src/useSize/useSize.tsx
@@ -0,0 +1,34 @@
+import { useState, useLayoutEffect } from 'react'
+type Size = { width?: number; height?: number }
+
+function useSize(target): Size {
+  const [state, setState] = useState<Size>(() => {
+    const el = target
+    return {
+      width: el.clientWidth,
+      height: el.clientHeight,
+    }
+  })
+
+  useLayoutEffect(() => {
+    const el = target
+    if (!el) {
+      return () => {}
+    }
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setState({
+          width: entry.target.clientWidth,
+          height: entry.target.clientHeight,
+        })
+      })
+    })
+    resizeObserver.observe(el)
+    return () => {
+      resizeObserver.disconnect()
+    }
+  }, [target])
+  return state
+}
+
+export default useSize
